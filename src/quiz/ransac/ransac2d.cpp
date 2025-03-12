d5c7d26597fa4a614b131b38f11f3a04f21464f5
MitchellUsayiwevu <mitchell.usayiwevu@student.uts.edu.au>
2025-03-12 16:17:09 +1100

    ransac 2d code

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -6,6 +6,9 @@
 #include "../../processPointClouds.h"
 // using templates for processPointClouds so also include .cpp to help linker
 #include "../../processPointClouds.cpp"
+#include <random>
+#include <iostream>
+#include <cmath>
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData()
 {
@@ -61,12 +64,72 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
   	return viewer;
 }
 
+void fitLine(double x1, double y1, double x2, double y2, double& A,double &B, double &C ){
+
+}
+
 std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
 {
 	std::unordered_set<int> inliersResult;
+    std::vector<double>line_eqn;
 	srand(time(NULL));
-	
+
+    std::mt19937 generator(int(std::time(0)));
+    std::uniform_int_distribution<int> dist(0, cloud->points.size() - 1);
+    std::unordered_set<int> samples;
+    float x1,y1,x2,y2,x3,y3, A, B, C;
+    float distance ;
+
 	// TODO: Fill in this function
+
+    for (int i=0; i<maxIterations; i++){
+
+        // use set as the container for samples because the 2 points need to be unique and set won't allow repeating the same sample twice.
+        while( samples.size() < 2 ){
+            samples.insert(dist(generator));
+        }
+
+        auto itt = samples.begin();
+        x1 = cloud->points[*itt].x;
+        y1 = cloud->points[*itt].y;
+        itt++;
+        x2 = cloud->points[*itt].x;
+        y2 = cloud->points[*itt].y;
+
+        A = y2-y1;
+        B = x1-x2;
+        C = ( (x2*y1)-(x1*y2) );
+
+        for (int j=0; j<cloud->points.size(); j++){
+
+            if (samples.count(j)>0){
+                continue;
+            }
+
+            x3 = cloud->points[j].x;
+            y3 = cloud->points[j].y;
+
+            distance = fabs( (A*x3)+(B*y3)+C) / sqrt( pow(A,2) + pow(B,2) );
+
+            if (distance < distanceTol){
+                samples.insert(j);
+            }
+
+
+        }
+
+        if(inliersResult.size()<samples.size()){
+            inliersResult = samples;
+            line_eqn.clear();
+            line_eqn.push_back(A);
+            line_eqn.push_back(B);
+            line_eqn.push_back(C);
+        }
+
+        return inliersResult;
+
+    }
+
 
 	// For max iterations 
 
@@ -92,7 +155,7 @@ int main ()
 	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 0, 0);
+	std::unordered_set<int> inliers = Ransac(cloud, 100, 1.0);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
